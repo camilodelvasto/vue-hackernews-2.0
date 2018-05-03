a71dc5f3f5ff341462550ee0b0c369ea753763e1
Camilo Delvasto <camilodelvasto@gmail.com>
2018-05-03 00:25:18 -0500

    Deploy

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ export function createRouter () {
     routes: [
       {
         path: '/nonprofits/:ein',
-        name: 'Nonprofits',
+        name: 'GenericNonprofit',
         component: GenericNonprofit
       }
     ]
